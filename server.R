c061d6ad01ec4490e9067f2f367dd13763b29aaf
joshuaadampoirier <josh.poirier@gmail.com>
2016-03-14 01:36:38 -0600

    Accepts lat/lon, date, elev inputs - no calculations yet

--- server.R
+++ server.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 a <- 6371.2             # reference radius of Earth
 
 # following radii (A & B) are used for transforming between geodetic and geocentric coordinates
@@ -5,6 +7,37 @@ A <- 40680631.59 ^ 0.5  # semi-major axe of reference spheroid (equatorial radiu
 B <- 40408299.89 ^ 0.5  # semi-minor axe of reference spheroid (polar radius of Earth, WGS84)
 
 shinyServer(
-    function(input, output) {
+    function(input, output, session) {
+        output$map <- renderPlot({
+            # get user input
+            lat <- input$Latitude
+            lon <- input$Longitude
+                        
+            # plot map of world with user-selected lat/long
+            map <- borders("world", colour="#5762AC", fill="#51BE69", alpha=0.6)
+            ggplot() + map + scale_x_continuous(limits=c(-180,180), expand=c(0,0)) + 
+                scale_y_continuous(limits=c(-90,90), expand=c(0,0)) +
+                geom_vline(xintercept=lon, colour="#D71400") + 
+                geom_hline(yintercept=lat, colour="#D71400") +
+                xlab("Longitude") + ylab("Latitude") + labs(title="IGRF Location") +
+                theme(legend.position="none")
+            
+        }, height=360, width=720)
         
-        })
+        output$selectedDate <- renderPrint(input$IGRFDate)
+        
+        observe({
+            # get user-selected lat/lon via clicking the map
+            lat <- input$map_click$y
+            lon <- input$map_click$x
+            
+            # update the latitude slider with map click value
+            updateSliderInput(session, "Latitude",
+                              value = lat)    
+            
+            # update the longitude slider with map click value
+            updateSliderInput(session, "Longitude",
+                              value = lon)
+        })       
+    }
+)
--- ui.R
+++ ui.R
@@ -1,12 +1,30 @@
 shinyUI(pageWithSidebar(
-    headerPanel("Example plot"),
+    # Application title
+    headerPanel("IGRF Calculator"),
+    
+    # User inputs
     sidebarPanel(
+        
+        # latitude input
         sliderInput('Latitude', 'Input Latitude', value=45,
                     min=-90, max=90, step=0.05),
+        
+        # longitude input
         sliderInput('Longitude', 'Input Longitude', value=45,
-                    min=-180, max=180, step=0.05)
+                    min=-180, max=180, step=0.05),
+        
+        # date input
+        dateInput('IGRFDate', 'Select Date', Sys.Date(), min='1900-01-01', max='2019-12-31', width='100%'),
+        
+        # elevation input
+        sliderInput('Elevation', 'Input Elevation (km above sea level)', value=0,
+                    min=-10, max=31856, step=0.1)
         ),
+    
+    # Map and outputs
     mainPanel(
-        h3('Hello world')
+        plotOutput('map', click="map_click"),
+        verbatimTextOutput("selectedDate"),
+        verbatimTextOutput("latlon")
         )
     ))
